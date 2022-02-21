fea96263df26008a940331047af09f39eaabd530
Helena <nenafgreco@hotmail.com>
2022-02-21 10:02:36 -0300

    modificando arq. charactersAPI

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT } = process.env;
+
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = REACT_APP_HAWKINS_URL,
+    timeout = REACT_APP_HAWKINS_TIMEOUT || DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
